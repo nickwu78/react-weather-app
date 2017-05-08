4e7527b05a9f3e91a066ae42ac267593f760f696
Nick Wu <nickwu78@gmail.com>
2017-05-08 22:50:04 +1000

    update server.js

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-// Redirect all https traffic to http (for openWeatherMap API)
-app.use(function (req, res, next) {
+app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
-    res.redirect('https://' + req.hostname + req.url);
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
 });
